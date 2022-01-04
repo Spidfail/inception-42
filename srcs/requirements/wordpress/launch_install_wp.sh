4c436e967efda7ecc1b704098f386c4c6a19ff88
Spidfail <guillian.hernandez@gmail.com>
2022-01-04 09:21:11 -0800

    inception : setup .env files && Tests
    [service].env files have env var as content.
    It's working.
    
    Tests : modification of wp pages, logs (users) on wordpress,
    persistance of modification between 'docker-compose down/up' cmds
    such as comment or page modifications.

--- srcs/requirements/wordpress/launch_install_wp.sh
+++ srcs/requirements/wordpress/launch_install_wp.sh
@@ -8,7 +8,7 @@ then
 	wp cli update
 	wp core download --path=/var/www/html/wordpress --allow-root
 	wp core config --path=/var/www/html/wordpress --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbprefix=wp_ --allow-root
-	wp core install --path=/var/www/html/wordpress --url="guhernan.42.fr" --title="Mot PReSSe" --admin_user=$WORDPRESS_DB_USER --admin_password=$WORDPRESS_DB_PASSWORD --admin_email="monemail@email.com" --allow-root
+	wp core install --path=/var/www/html/wordpress --url="guhernan.42.fr" --title="Mot Presse" --admin_user=$WORDPRESS_DB_USER --admin_password=$WORDPRESS_DB_PASSWORD --admin_email="monemail@email.com" --allow-root
 fi
 
 exec php-fpm7 -F -R
