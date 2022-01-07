b56c4058119bad2a038c63ea8d31a552dd1de5f8
guhernan <Spidfail@github.com>
2022-01-07 10:59:43 -0800

    inception : wp -> manage users and comments
    WP : add user - user
    Need to be log to comment posts
    
    Still :
    Dockerignores !

--- srcs/requirements/wordpress/launch_install_wp.sh
+++ srcs/requirements/wordpress/launch_install_wp.sh
@@ -9,6 +9,12 @@ then
 	wp core download --path=/var/www/html/wordpress --allow-root
 	wp core config --path=/var/www/html/wordpress --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbprefix=wp_ --allow-root
 	wp core install --path=/var/www/html/wordpress --url="guhernan.42.fr" --title="Mot Presse" --admin_user=$WORDPRESS_DB_USER --admin_password=$WORDPRESS_DB_PASSWORD --admin_email="monemail@email.com" --allow-root
+	# Possible setup :	users can register with email address.
+	# 			some configuration is required to make it works.
+	#			For example : doesn't send email at this stage.
+	wp option update users_can_register 1 --allow-root
+	wp option update comment_registration 1 --allow-root
+	wp user create user user@guhernan.42lyon.fr --role=subscriber --user_pass=user --user_registered=2022-01-07
 fi
 
 exec php-fpm7 -F -R
